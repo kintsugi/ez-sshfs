08ceef0a814b30158c47e135fce273e38f2acf06
Kyle Salter <ksalter@ucsc.edu>
2017-01-30 15:41:23 -0800

    removed test files

--- scripts/sshfs_sshfs-test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo sshfs -o allow_other,default_permissions root@107.170.248.175:/root/test /Users/terminull/dev/proj/ez-sshfs/test-remote
--- scripts/sshfs_sshfs-test_remote.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo sshfs -o allow_other,default_permissions terminull@turing.kylesalter.com:/Users/terminull/dev/proj/ez-sshfs/test-remote /root/test
